2728b11cb57809551fe4cdc1f22fda29ebd44bf4
Matt Carrier <icecreammatt@gmail.com>
2015-08-18 01:28:44 -0700

    Fetch lights list from API instead of hard coding

--- src/components/LightClientApp.js
+++ src/components/LightClientApp.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('whatwg-fetch');
 var React = require('react/addons');
 var ReactTransitionGroup = React.addons.TransitionGroup;
 var Lightswitch = require('./Lightswitch');
@@ -8,6 +9,10 @@ let ThemeManager = new mui.Styles.ThemeManager();
 let Ledcontroller = require('./Ledcontroller');
 let injectTapEventPlugin = require('react-tap-event-plugin');
 
+let ip = '192.168.1.11';
+let port = '5000';
+let baseUrl = `http://${ ip }:${ port }/`;
+
 injectTapEventPlugin();
 
 // CSS
@@ -15,6 +20,22 @@ require('normalize.css');
 require('../styles/main.css');
 
 var LightClientApp = React.createClass({
+
+  getInitialState() {
+    return {items: []};
+  },
+
+  componentDidMount() {
+    let url = `${ baseUrl }lights`;
+    fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      console.log(json);
+      this.setState({items: json});
+    });
+  },
 
   childContextTypes: {
     muiTheme: React.PropTypes.object
@@ -27,11 +48,16 @@ var LightClientApp = React.createClass({
   },
 
   render: function() {
+    var switches = this.state.items.map(light => {
+      return (
+        <Lightswitch lightlabel={light.Username} lightid={light.Id} active="false" />
+      );
+    });
+
     return (
       <div className="main">
         <ReactTransitionGroup transitionName="fade">
-          <Lightswitch lightlabel="mobile" lightid="1" active="false"/>
-          <Lightswitch lightlabel="entry" lightid="2" active="false"/>
+          {switches}
           <Ledcontroller />
         </ReactTransitionGroup>
       </div>
